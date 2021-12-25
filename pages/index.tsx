3b6c421d2e8e05701499a2539a526596f10128e0
katashin <katachanpe0513@icloud.com>
2021-12-26 04:47:03 +0900

    meta_test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { PageHead } from '../components/_document';
 import { Header } from '../components/header/Header';
 import { HeaderMain } from '../components/header/HeaderMain';
 import { About } from '../components/about/about';
@@ -13,6 +14,7 @@ import { FuuterBar } from '../components/futter/futter';
 const Home: NextPage = () => {
   return (
     <div className='contents tracking-wider text-sm leading-5'>
+      <PageHead />
       <Header />
       <HeaderMain />
       <About />
