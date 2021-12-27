2e9eb08ea94b59d902ca2db5cabb46ad847ad4ce
katashin <katachanpe0513@icloud.com>
2021-12-27 14:54:31 +0900

    sharebutton

--- components/_document.tsx
+++ components/_document.tsx
@@ -15,6 +15,7 @@ export const PageHead = () => {
       <meta property="og:image" content="https://lh3.googleusercontent.com/fGor363bamt0DbdWxBJAy5GNJeL18tlDr54C247pKRGFDRQmT9qcWSOwy_PEjZYE7_ogntJ7RwomB1CyoFMkdKoJvbLzn5V-x4WRVZfJ8c5PBxEUK3Mx-wphiRzCD_rdQ1aNgX2tF7uoVN8e5CaOEyyYxuAMBN_ylkABiKFoPnKMxq20c3Pd9FPjbUpk-OKP7RBHNMSv9evBv2a5LsQTm1NeaiLCcs8JZEuobyxMGdsEvRzHH8ZEc4OHLaOdc_yJHbqaJUa1iaVks0yZ-HUGvQpQZBuvNO4q6PZRTDb8F77zGc8qI_IRdIH5dznhr5LzxUeLvYwmAjFrLwaIfgc4PphhjuTk4K4c9aMeUwiMhvdDAsrAeW8qtnkVEiYzBvn9MzNHXT7tTq6j4pxBaElAw9wZFahfxlPYJ_m8f6VhgX_ol-oenKNQlbZJogKanyn8LvPX6npGPO2F9nwqAYHwjKL7-N-vGN30Cg1ovKJeb5UbwoSwh6PM0Koi6Q7Dxnk92V48xUWyRCfkd1cfQ6EuMoCs6uKjzK5dPdZkDtWT8zk8RZ4dUFlOGQYcEXVWdxBBXUNWUstVdY4fxr0YdxdnYNNTKX3_9oqCsqVJYk26cXatrgee-sScuNW-2j4UwCBZFqMrt-ReMqjOq5Jx3CI-C-z76vdEjZv6Ps-F-klWPnSIwvdzjFRaY0eVVeCn1rJExS8qeASI_diAT235tcEsf8A=w1280-h512-no?authuser=0" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@katashin_3812" />
+      {/* <meta property="fb:app_id" content='' /> */}
       <title>My Profile</title>
       <link rel="icon" href="favicon.ico" />
     </Head>
