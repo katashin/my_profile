831170a1cbaa9e14a1ad875e6f6c45a52b40a75a
katashin <katachanpe0513@icloud.com>
2021-12-27 15:20:06 +0900

    testshare

--- components/futter/futter.tsx
+++ components/futter/futter.tsx
@@ -10,11 +10,14 @@ export const FuuterBar = () => {
   return (
     <div className='bg-futter-black text-white text-center'>
       <div className="py-10">
-        <p className='uppercase'>{text.create}　 <span className="text-xl font-bold">{text.name}</span></p>
+        <p className='uppercase'>{text.create} <span className="text-xl font-bold">{text.name}</span></p>
         <p className="py-10 hover:opacity-75"><Image src='/logo_white.png' width={100} height={120} alt="フッター画像" /></p>
         <div className="flex justify-center pb-10">
           <div className="pr-5 hover:opacity-75">
-            <Image src='/twitter.png' width={30} height={30} alt="フッター画像" />
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-show-count='false' target={'_blank'} data-text='シェアありがとうございます。片山真介と申します。宜しくお願いします。' data-hashtags='fwywd'>
+              <Image src='/twitter.png' width={30} height={30} alt="フッター画像" />
+              <script async src='https://platform.twitter.com/widgets.js' charSet='utf-8'></script>
+            </a>
           </div>
           <div className="hover:opacity-75">
             <Image src='/facebook.png' width={30} height={30} alt="フッター画像" />
