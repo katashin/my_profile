253026fdd44a1a8c774baf13de1f238f7371cebf
katashin <katachanpe0513@icloud.com>
2022-01-03 16:31:59 +0900

    tailwind css の設定を見直す

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ['./src/**/*.tsx', './src/**/*.tsx'],
   darkMode: false, // or 'media' or 'class'
   content: [],
   theme: {
