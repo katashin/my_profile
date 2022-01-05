89f3d276ece1045c00fe6812ab9e674072163905
katashin <katachanpe0513@icloud.com>
2022-01-06 02:45:08 +0900

    レイアウトの見直し

--- src/components/Pageheader/Pagehead.tsx
+++ src/components/Pageheader/Pagehead.tsx
@@ -4,67 +4,65 @@ import { useState } from 'react';
 import { Link as Scroll } from 'react-scroll';
 
 export const Header = () => {
-  const menuLists: string[] = ['about', 'skills', 'values', 'future'];
+  const menus: string[] = ['about', 'skills', 'values', 'future'];
   const [openMenu, setOpenMenu] = useState(false);
 
   return (
-    <div id='header' className='px-2 bg-white border-b-2'>
-      <div className='justify-between md:flex'>
+    <div id='header' className='mx-auto max-w-[90%]'>
+      <div className='justify-between items-center cursor-pointer md:flex'>
         <div className='text-center'>
           <a href='https://fwywd.com/' target={'_blank'} rel='noreferrer'>
             <Image
-              className='hover:opacity-75'
+              className='hover:opacity-70'
               src='/logo.png'
               alt='ヘッダーロゴ'
               width={150}
-              height={80}
+              height={75}
             />
           </a>
         </div>
-        <div className='flex justify-between'>
-          {menuLists.map(function (value, index) {
+        <div className='hidden justify-between md:flex'>
+          {menus.map(function (menu, index) {
             return (
-              <ul key={index} className='hidden my-auto cursor-pointer md:flex'>
-                <Scroll to={value} smooth={true} duration={600}>
-                  <li
-                    key={index}
-                    className='font-bold hover:text-font-green uppercase md:px-5'
-                  >
-                    {value}
-                  </li>
+              <nav key={index} className='mx-5 font-bold hover:text-font-green uppercase'>
+                <Scroll to={menu} smooth={true} duration={600}>
+                  {menu}
                 </Scroll>
-              </ul>
+              </nav>
             );
           })}
         </div>
-        {/* ハンバーガーメニュー */}
-        <button
-          onClick={() => setOpenMenu(true)}
-          className='absolute top-2 left-2 flex-initial md:hidden'
-        >
-          <Image src='/hangmenu.png' alt='menu' width={30} height={30}></Image>
-        </button>
-        {openMenu ? (
-          <div className='absolute top-0 left-1 z-10 flex-initial bg-white md:hidden'>
-            {menuLists.map(function (value, index) {
-              return (
-                <>
-                  <Scroll to={value} smooth={true} duration={600}>
-                    <div className='w-16 border-b'>
-                      <button
-                        onClick={() => setOpenMenu(false)}
-                        className='font-bold hover:text-font-green uppercase cursor-pointer'
-                      >
-                        {value}
-                      </button>
-                    </div>
-                  </Scroll>
-                </>
-              );
-            })}
-          </div>
-        ) : null}
       </div>
     </div>
+
+    //     {/* ハンバーガーメニュー */}
+    //     <button
+    //       onClick={() => setOpenMenu(true)}
+    //       className='absolute top-2 left-2 flex-initial md:hidden'
+    //     >
+    //       <Image src='/hangmenu.png' alt='menu' width={30} height={30}></Image>
+    //     </button>
+    //     {openMenu ? (
+    //       <div className='absolute top-0 left-1 z-10 flex-initial bg-white md:hidden'>
+    //         {menus.map(function (value, index) {
+    //           return (
+    //             <>
+    //               <Scroll to={value} smooth={true} duration={600}>
+    //                 <div className='w-16 border-b'>
+    //                   <button
+    //                     onClick={() => setOpenMenu(false)}
+    //                     className='font-bold hover:text-font-green uppercase cursor-pointer'
+    //                   >
+    //                     {value}
+    //                   </button>
+    //                 </div>
+    //               </Scroll>
+    //             </>
+    //           );
+    //         })}
+    //       </div>
+    //     ) : null}
+    //   </div>
+    // </div>
   );
 };
