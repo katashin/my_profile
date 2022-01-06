c78efb12d2fb829cf62e17c955a637f6195960e2
katashin <katachanpe0513@icloud.com>
2022-01-07 07:40:43 +0900

    modeshi4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,15 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { ShareButton } from '../components/ShareButton';
+import { PageHead } from '../components/_document';
+import { About } from '../components/about/aboutme';
 import { Future } from '../components/future/futureme';
 import { FuuterBar } from '../components/pagefooter/PageFooterBar';
 import { HeaderMain } from '../components/pageheader/Hero';
 import { Header } from '../components/pageheader/pagehead';
-import { ShareButton } from '../components/ShareButton';
 import { Skill } from '../components/skills/skill';
 import { Value } from '../components/values/Value';
-import { PageHead } from '../components/_document';
-import { About } from '../components/about/aboutme';
 // import styles from '..../styles/Home.module.css';
 
 const Home: NextPage = () => {
